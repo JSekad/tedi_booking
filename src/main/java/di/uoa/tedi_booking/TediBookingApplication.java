aeba4be6ad3af38db99d21a0de4a6a8d43a54124
jsekad <jsekad@gmail.com>
2023-04-08 21:33:06 +0300

    init commit

---
+++ src/main/java/di/uoa/tedi_booking/TediBookingApplication.java
@@ -0,0 +1,13 @@
+package di.uoa.tedi_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TediBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TediBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/di/uoa/tedi_booking/TediBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package di.uoa.tedi_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TediBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
