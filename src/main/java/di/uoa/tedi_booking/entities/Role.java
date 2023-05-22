f020268e26883c28e74d8b03ef8f9e2487397257
jsekad <jsekad@gmail.com>
2023-05-22 15:37:27 +0300

    Modify jwt and login page

--- src/main/java/di/uoa/tedi_booking/entities/Role.java
+++ src/main/java/di/uoa/tedi_booking/entities/Role.java
@@ -1,5 +1,6 @@
 package di.uoa.tedi_booking.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 public class Role implements Serializable {
 
     @Id
+    @JsonIgnore
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
