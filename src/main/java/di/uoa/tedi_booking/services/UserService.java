ceea273f6f6c76cf88d3607d095212879cb6d9f5
bill <vtsimbos@hotmail.com>
2023-05-23 20:37:19 +0300

    update

--- src/main/java/di/uoa/tedi_booking/services/UserService.java
+++ src/main/java/di/uoa/tedi_booking/services/UserService.java
@@ -2,13 +2,19 @@ package di.uoa.tedi_booking.services;
 
 import di.uoa.tedi_booking.entities.User;
 import di.uoa.tedi_booking.repositories.GenericRepository;
+import di.uoa.tedi_booking.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService extends GenericService<User>{
 
+    private final UserRepository userRepository;
+
     @Autowired
-    public UserService(GenericRepository<User> repository) { super(repository); }
+    public UserService(UserRepository repository) {
+        super(repository);
+        this.userRepository = repository;
+    }
 
 }
