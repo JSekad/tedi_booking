5338180a643642dd14a45339c47584f6dc466c25
bill <vtsimbos@hotmail.com>
2023-05-23 20:34:36 +0300

    update

--- src/main/java/di/uoa/tedi_booking/controller/RoomController.java
+++ src/main/java/di/uoa/tedi_booking/controller/RoomController.java
@@ -3,8 +3,11 @@ package di.uoa.tedi_booking.controller;
 import di.uoa.tedi_booking.entities.Room;
 import di.uoa.tedi_booking.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/room")
@@ -16,6 +19,11 @@ public class RoomController extends GenericController<Room>{
     public RoomController(RoomService service){
         super(service);
         this.roomService = service;
+    }
+
+    @GetMapping(path = "/searchAvailability/{area}/{startDate}/{endDate}/{numOfPersons}")
+    public @ResponseBody List<Room> searchAvailableRooms(@PathVariable String area, @PathVariable LocalDate startDate, @PathVariable LocalDate endDate, @PathVariable Long numOfPersons){
+        return roomService.searchAvailableRooms(area, startDate, endDate, numOfPersons);
     }
 
 }
